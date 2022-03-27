088161f9915fde1256118090b03a7d5db57444f0
khadeer <shaikkafil01@gmail.com>
2022-03-27 08:28:17 +0300

    Add files via upload

---
+++ sale_auto_invoice/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sales Auto Invoice',
+    'version': '1.0.0',
+    'category': 'Hidden',
+    'author': 'Abderrahmane Guessoum - Alpha Brains Technologies',
+    'website': 'www.fiverr.com/aabcrow',
+    'depends': [
+        'sale_management',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'LGPL-3',
+    
+}
